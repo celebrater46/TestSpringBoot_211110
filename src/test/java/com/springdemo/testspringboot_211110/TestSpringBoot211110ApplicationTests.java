c9e0ab17733d6a9669a0b3cd0751fd3d2a4b629e
hideru <jota.16.stigma.world@gmail.com>
2021-11-10 12:53:18 +0900

    first commit

---
+++ src/test/java/com/springdemo/testspringboot_211110/TestSpringBoot211110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.testspringboot_211110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBoot211110ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
